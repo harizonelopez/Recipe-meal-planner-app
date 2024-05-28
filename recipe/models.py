f86e2d53f6505264efc3d0cbef501456fd3767c9
Harizone lopez <harizonelopez23@gmail.com>
2024-05-28 13:51:50 +0300

    commit

--- recipe/models.py
+++ recipe/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import User
 class Recipe(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
-    day = models.CharField(max_length=50)
     ingredients = models.TextField()
     instructions = models.TextField()
 
